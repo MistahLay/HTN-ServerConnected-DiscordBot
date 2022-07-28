e3630bc120ec2f2c32d5d34c8ef7d97559661681
LayWasTaken <clarencetoledo08@gmail.com>
2022-07-28 19:13:40 +0800

    Added a coordinate model

---
+++ src/Utils/CoordinateModel.ts
@@ -0,0 +1,13 @@
+import { ObjectModel } from "objectmodel"
+
+export interface CoordinateInterface {
+    x: number,
+    y: number,
+    z: number
+}
+
+export const Model = {
+    x: Number,
+    y: Number,
+    z: Number
+}
