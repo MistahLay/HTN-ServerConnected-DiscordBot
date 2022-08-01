090a50dd823f3a03cde7599fd5ed4e4c70f103ee
LayWasTaken <clarencetoledo08@gmail.com>
2022-08-01 23:38:40 +0800

    Changed Command Registering

---
+++ src/BotCommand.ts
@@ -0,0 +1,34 @@
+import { APIEmbed, Colors, Message } from "discord.js";
+import fs from 'fs';
+export class Command {
+    info: string = "";
+    isInvisible:boolean = false;
+    execute?:(msg: Message, args: string[]) => void;
+    paramType:{name:string, type:string}[] = [];
+    onExecute(execute:(msg: Message, args: string[]) => void){this.execute = execute; return this;};
+    setInfo(info: string) {this.info = info; return this};
+    setParamType(types:{name:string, type:string}[]){this.paramType = types; return this;}
+    setInvisible(set:boolean){this.isInvisible = set; return this;}
+}
+
+export let commandsInfo:APIEmbed = {
+    title: "Commands",
+    description: "Note: When the server is down most of the commands will not work ofc. \n-LayWasTaken",
+    color: Colors.Green,
+    fields: []
+};
+
+export const commands:{[key:string]: Command} = {}
+
+export async function getCommands():Promise<{[key:string]: Command}>{
+    return new Promise(r => {
+        fs.readdirSync(__dirname+"/Commands", {withFileTypes: true})
+        .filter(value => value.isFile())
+        .forEach(value => {
+            const cmd = require(__dirname+"/Commands/"+value.name);
+            if(!(cmd instanceof Command)) return;
+            commands[value.name.replace(".ts", "").toLowerCase()] = cmd;
+        });
+        r(commands);
+    })
+}
