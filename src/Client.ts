f9edd57b3a6986f5679591ba21a0eb8107250c6a
LayWasTaken <clarencetoledo08@gmail.com>
2022-08-26 12:20:52 +0800

    fixed database

--- src/Client.ts
+++ src/Client.ts
@@ -2,19 +2,21 @@ import { ActivityType, Client, Colors, Message } from "discord.js";
 import { commands, registerCommands } from "./BotCommand";
 import { SocketConnection } from "./SocketConnection";
 import mysql from "mysql";
-require("dotenv").config({ path: __dirname + "/.env" });
+import dotenv from "dotenv";
+dotenv.config({ path: __dirname + "/.env" });
 const token = process.env.TOKEN;
 
 export const client = new Client({
     intents: ["Guilds", "GuildMessages", "DirectMessages", "MessageContent"],
 });
 export const socket = new SocketConnection();
-export const db = mysql.createConnection({
-    host: "localhost",
+export const db = mysql.createPool({
+    host: "127.0.0.1",
     port: 3306,
     database: "ServerDatabase",
     user: "sqluser",
     password: "password",
+    multipleStatements: true,
 });
 socket.on("Connected", () => {
     client.user?.setActivity("Main Server Socket", {
@@ -27,12 +29,12 @@ socket.on("Disconnect", () => {
     console.log("Disconnected");
 });
 client.once("ready", async () => {
-    socket.handleConnection();
+    client.user?.setActivity();
+    socket.handleConnection(true);
     await registerCommands();
     require("./PMMPServerStatus");
     console.log("Bot Online");
 });
-
 client.on("messageCreate", async (msg) => {
     if (msg.channelId === require("./channels.json").StaffServer.ServerStatus) {
         await msg.delete();
@@ -42,17 +44,17 @@ client.on("messageCreate", async (msg) => {
     await CommandValidator(msg);
 });
 async function CommandValidator(msg: Message) {
-    if (!msg.content.startsWith("c?")) return;
-    const args = msg.content.slice(2).split(" ");
+    if (!msg.content.startsWith(">")) return;
+    const args = msg.content.slice(1).split(" ");
     const cmd = commands[args[0].toLowerCase()];
     args.shift();
     if (!cmd) {
-        msg.reply({
+        return msg.reply({
             embeds: [
                 {
                     title: "Command Error",
                     color: Colors.Red,
-                    description: `Command doesnt exist pls do c?help for the list of valid commands`,
+                    description: `Command doesnt exist pls do >help for the list of valid commands`,
                 },
             ],
         });
@@ -73,7 +75,7 @@ async function CommandValidator(msg: Message) {
                     description:
                         "Command is missing " +
                         (cmd.paramType.length - args.length) +
-                        " arguments pls do ``c?help <command>`` to get more info about the command",
+                        " arguments pls do ``>help <command>`` to get more info about the command",
                 },
             ],
         });
