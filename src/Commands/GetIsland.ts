e0f8ddafd4cbf0a95198b79f319ba1cdbf170f09
LayWasTaken <clarencetoledo08@gmail.com>
2022-08-30 21:23:34 +0800

    Renamed

--- src/Commands/GetIsland.ts
+++
@@ -1,6 +0,0 @@
-import { Command } from "../BotCommand";
-
-module.exports = new Command()
-   .setInfo("This will ban a player in the ingame server")
-   .setParamType([{ name: "islandID", type: "string" }])
-   .onExecute((msg, [id]) => {});
--- src/Commands/GetPlayer.ts
+++
@@ -1,6 +0,0 @@
-import { Command } from "../BotCommand";
-
-module.exports = new Command()
-    .setInfo("This will ban a player in the ingame server")
-    .setParamType([{ name: "player", type: "string" }])
-    .onExecute((msg, [player]) => {});
