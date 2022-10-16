d96318e4cd5ed1e6054c7c35c94e4a6379cec698
LayWasTaken <clarencetoledo08@gmail.com>
2022-10-16 14:59:29 +0800

    Fixed initialization

--- src/Client.ts
+++ src/Client.ts
@@ -1,15 +1,14 @@
-import { ActivityType, Client, Colors, Message, TextChannel } from "discord.js";
+import { APIEmbedField, Client, Message, TextChannel } from "discord.js";
 import { commands, registerCommands } from "./BotCommand";
-import { SocketConnection } from "./SocketConnection";
 import mysql from "mysql";
 import dotenv from "dotenv";
+import { ServerSocket } from "./ServerSocket";
 
 dotenv.config({ path: __dirname + "/.env" });
 const token = process.env.TOKEN;
 export const client = new Client({
     intents: ["Guilds", "GuildMessages", "DirectMessages", "MessageContent"],
 });
-export const socket = new SocketConnection();
 export const db = mysql.createPool({
     host: "127.0.0.1",
     port: 3306,
@@ -19,106 +18,48 @@ export const db = mysql.createPool({
     multipleStatements: true,
 });
 export const channels: { [key: string]: TextChannel } = {};
-socket.on("Connected", () => {
-    client.user?.setActivity("Main Server Socket", {
-        type: ActivityType.Listening,
-    });
-    console.log("Connected");
-});
-socket.on("Disconnect", () => {
-    client.user?.setActivity();
-    console.log("Disconnected");
-});
-client.once("ready", async () => {
+export const socket = new ServerSocket();
+client.on("ready", async () => {
     await registerChannels();
-    client.user?.setActivity();
-    socket.handleConnection(true);
     await registerCommands();
-    require("./PMMPServerStatus");
-    console.log("Bot Online");
+    console.log("Bot is ready");
 });
+const s = require("./channels.json").StaffServer as { [key: string]: string };
 client.on("messageCreate", async (msg) => {
-    if (msg.channelId === require("./channels.json").StaffServer.ServerStatus) {
+    if (s[msg.channelId]) {
         await msg.delete();
         return;
     }
     if (msg.author.bot) return;
     await CommandValidator(msg);
 });
+// prettier-ignore
+function commandError(msg:Message, reason: string, fields: APIEmbedField[] = []){
+    msg.reply({embeds:[{
+        title: "Command Error",
+        description: reason,
+        fields
+    }]});
+}
+// prettier-ignore
 async function CommandValidator(msg: Message) {
     if (!msg.content.startsWith(">")) return;
-    const args = msg.content.slice(1).split(" ");
-    const cmd = commands[args[0].toLowerCase()];
-    args.shift();
-    if (!cmd) {
-        return msg.reply({
-            embeds: [
-                {
-                    title: "Command Error",
-                    color: Colors.Red,
-                    description: `Command doesnt exist pls do >help for the list of valid commands`,
-                },
-            ],
-        });
-    }
-    if (!Object.hasOwn(cmd, "paramType")) {
-        cmd.execute?.(msg, args);
-        return;
-    }
-    if (
-        !(cmd.paramType.length === 0) &&
-        !(cmd.paramType.length <= args.length)
-    ) {
-        msg.reply({
-            embeds: [
-                {
-                    title: "Command Error",
-                    color: Colors.Red,
-                    description:
-                        "Command is missing " +
-                        (cmd.paramType.length - args.length) +
-                        " arguments pls do ``>help <command>`` to get more info about the command",
-                },
-            ],
-        });
-        return;
-    }
-    for (let index = 0; index < cmd.paramType.length; index++) {
-        switch (cmd.paramType[index].type) {
-            case "number":
-                if (isNaN(args[index] as any))
-                    return msg.reply({
-                        embeds: [
-                            {
-                                title: "Command Error",
-                                color: Colors.Red,
-                                description: `Parameter <${cmd.paramType[index].name}> must be a number, given string`,
-                            },
-                        ],
-                    });
-                break;
-            case "string":
-                break;
-            default:
-                if (!cmd.paramType[index].type.includes(args[index]))
-                    return msg.reply({
-                        embeds: [
-                            {
-                                title: "Command Error",
-                                color: Colors.Red,
-                                description: `Parameter <${
-                                    cmd.paramType[index].name
-                                }> must be ${cmd.paramType[
-                                    index
-                                ].type.toString()} of these, given ${
-                                    args[index]
-                                }`,
-                            },
-                        ],
-                    });
-                break;
-        }
-    }
+    let args = msg.content.slice(1).split(" ");
+    if(!args[0]) return commandError(msg, "Pls input a command")
+    const cmd = commands[args.shift()?.toLowerCase() ?? "unknown"];
+    if(!cmd) return commandError(msg, "Command doesn't exists");
+    if(cmd.requiredServerOnline)
+        if(socket.isOnline()) return commandError(msg, "Command requires the ingame server to be online")
+    if(!(cmd.roles.length === 0))
+        if(await (new Promise((r) => {
+        let current = 0;
+        const roles = msg.member?.roles.cache.size;
+        msg.member?.roles.cache.forEach(role => {
+            if(cmd.roles.includes(role.id)) r(true);
+            if(current === roles) r(false);
+        })
+    }))) return commandError(msg, "Command cannot be used");
+    if(!(cmd.parameters.length === 0) && cmd.parameters.length <= args.length) return commandError(msg, `${cmd.parameters.length - args.length} arguments we're added, need ${cmd.parameters.length}`);
     await cmd.execute?.(msg, args);
 }
 async function registerChannels() {
